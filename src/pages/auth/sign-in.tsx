dea056357fd39873081e69ce10a61b6235dbe08d
Rodrigo <rodrigosanyy@gmail.com>
2024-02-22 10:33:00 -0300

    feat: add sonner notification toast

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -20,8 +21,19 @@ export function SignIn() {
   } = useForm<SignInForm>()
 
   async function handlerSignIn(data: SignInForm) {
-    console.log(data)
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+      toast.success('Enviamos um link de autenticação para seu email!', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => {
+            handlerSignIn(data)
+          },
+        },
+      })
+    } catch {
+      toast.error('Credenciais inválidas!')
+    }
   }
 
   return (
