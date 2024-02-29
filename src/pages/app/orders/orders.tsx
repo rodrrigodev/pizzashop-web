e94aca17a02be6d534b80ac68bf288fe13dfdb4b
Rodrigo <rodrigosanyy@gmail.com>
2024-02-29 09:55:10 -0300

    feat: some form components add

---
+++ src/pages/app/orders/orders.tsx
@@ -0,0 +1,50 @@
+import { Helmet } from 'react-helmet-async'
+
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+import { OrderTableRow } from './order-table-row'
+import { OrdersTableFilters } from './orders-table-filters'
+
+export function Orders() {
+  return (
+    <>
+      <Helmet title="Pedidos" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+      </div>
+
+      <div className="space-y-2.5">
+        <OrdersTableFilters />
+
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[64px]"></TableHead>
+                <TableHead className="w-[140px]">Identificador</TableHead>
+                <TableHead className="w-[180px]">Realizado</TableHead>
+                <TableHead className="w-[140px]">Status</TableHead>
+                <TableHead>Cliente</TableHead>
+                <TableHead className="w-[140px]">Total</TableHead>
+                <TableHead className="w-[164px]"></TableHead>
+                <TableHead className="w-[132px]"></TableHead>
+              </TableRow>
+            </TableHeader>
+
+            <TableBody>
+              {Array.from({ length: 10 }).map((_, i) => {
+                return <OrderTableRow key={i} />
+              })}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </>
+  )
+}
