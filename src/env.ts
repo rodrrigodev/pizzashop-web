5810fad33a4e1670fd8b8583f2aabad89dae3a0a
Rodrigo <101267761+rodrrigodev@users.noreply.github.com>
2024-03-19 13:29:00 +0000

    feat: loading states

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,8 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 const envSchema = z.object({
   VITE_API_URL: z.string().url(),
-})
+  VITE_ENABLR_API_DELAY: z.string().transform((value) => value === "true"),
+});
 
-export const env = envSchema.parse(import.meta.env)
+export const env = envSchema.parse(import.meta.env);
