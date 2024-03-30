940842a92f4a3a5164bae9e2e700b4917804d2a1
Rodrigo <101267761+rodrrigodev@users.noreply.github.com>
2024-03-30 12:52:31 +0000

    feat: filters orders working

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,5 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
+import { useSearchParams } from "react-router-dom";
+import { z } from "zod";
 
+import { getOrders } from "@/api/get-orders";
 import { Pagination } from "@/components/pagination";
 import {
   Table,
@@ -10,40 +14,47 @@ import {
 } from "@/components/ui/table";
 
 import { OrderTableRow } from "./order-table-row";
-import { OrdersTableFilters } from "./orders-table-filters";
-import { useQuery } from "@tanstack/react-query";
-import { getOrders } from "@/api/get-orders";
-import { useSearchParams } from "react-router-dom";
-import { z } from "zod";
+import { OrderTableFilters } from "./orders-table-filters";
 
 export function Orders() {
-  const [searchParams, setSeachParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const orderId = searchParams.get("orderId");
+  const customerName = searchParams.get("customerName");
+  const status = searchParams.get("status");
 
   const pageIndex = z.coerce
     .number()
     .transform((page) => page - 1)
-    .parse(searchParams.get("page") ?? "0");
+    .parse(searchParams.get("page") ?? "1");
 
   const { data: result } = useQuery({
-    queryKey: ["orders", pageIndex],
-    queryFn: () => getOrders({ pageIndex }),
+    queryKey: ["orders", pageIndex, orderId, customerName, status],
+    queryFn: () =>
+      getOrders({
+        pageIndex,
+        orderId,
+        customerName,
+        status: status === "all" ? null : status,
+      }),
   });
 
-  function handlerPaginate(pageIndex: number) {
-    setSeachParams((state) => {
+  function handlePaginate(pageIndex: number) {
+    setSearchParams((state) => {
       state.set("page", (pageIndex + 1).toString());
 
       return state;
     });
   }
+
   return (
     <>
       <Helmet title="Pedidos" />
+
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-
         <div className="space-y-2.5">
-          <OrdersTableFilters />
+          <OrderTableFilters />
 
           <div className="rounded-md border">
             <Table>
@@ -51,15 +62,14 @@ export function Orders() {
                 <TableRow>
                   <TableHead className="w-[64px]"></TableHead>
                   <TableHead className="w-[140px]">Identificador</TableHead>
-                  <TableHead className="w-[180px]">Realizado</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
                   <TableHead className="w-[140px]">Status</TableHead>
                   <TableHead>Cliente</TableHead>
-                  <TableHead className="w-[140px]">Total</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
                   <TableHead className="w-[164px]"></TableHead>
                   <TableHead className="w-[132px]"></TableHead>
                 </TableRow>
               </TableHeader>
-
               <TableBody>
                 {result &&
                   result.orders.map((order) => {
@@ -71,10 +81,10 @@ export function Orders() {
 
           {result && (
             <Pagination
+              onPageChange={handlePaginate}
               pageIndex={result.meta.pageIndex}
               totalCount={result.meta.totalCount}
               perPage={result.meta.perPage}
-              onPageChange={handlerPaginate}
             />
           )}
         </div>
